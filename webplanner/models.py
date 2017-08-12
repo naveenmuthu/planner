052efe9d38e519e2b0f1e2559ede80c43e492c49
Naveen Muthu <muthun@email.chop.edu>
2017-08-12 18:09:35 -0400

    Setting up model and testing display on index page

--- webplanner/models.py
+++ webplanner/models.py
@@ -3,3 +3,13 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class todo(models.Model):  # Table name, has to wrap models.Model to get the functionality of Django.
+
+    taskID = models.IntegerField(unique=True)
+    name = models.CharField(max_length=200)
+    description = models.TextField()  # Like a TEXT field
+    created = models.DateTimeField()  # Like a DATETIME fieldma
+
+    def __unicode__(
+            self):  # Tell it to return as a unicode string (The name of the to-do item) rather than just Object.
+        return self.name
--- webplanner/views.py
+++ webplanner/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
+from models import todo
 
 # Create your views here.
+
+def index(request):
+
+    items = todo.objects.all()
+
+    return render_to_response('index.html', {'items': items})
