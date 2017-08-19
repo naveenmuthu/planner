3bd0cf0a96a4fd0433abbecd8efef6cdafe9e5d5
Naveen Muthu <muthun@email.chop.edu>
2017-08-19 16:19:43 -0400

    Adding bootstrap

--- webplanner/models.py
+++ webplanner/models.py
@@ -2,14 +2,27 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+PRIORITY_CHOICES = (
+  (1, 'Low'),
+  (2, 'Normal'),
+  (3, 'High'),
+  (4, 'Top')
+)
+
 # Create your models here.
 class todo(models.Model):  # Table name, has to wrap models.Model to get the functionality of Django.
 
-    taskID = models.IntegerField(unique=True)
-    name = models.CharField(max_length=200)
-    description = models.TextField()  # Like a TEXT field
-    created = models.DateTimeField()  # Like a DATETIME fieldma
+    title = models.CharField(max_length=200)
+    created_date = models.DateField(auto_now=True)
+    start_date = models.DateField(blank=True, null=True, )
+    due_date = models.DateField(blank=True, null=True, )
+    completed = models.BooleanField(default=None)
+    completed_date = models.DateField(blank=True, null=True)
+    priority = models.PositiveIntegerField(choices=PRIORITY_CHOICES, default=2)
 
-    def __unicode__(
-            self):  # Tell it to return as a unicode string (The name of the to-do item) rather than just Object.
-        return self.name
+    class Meta:
+        ordering = ['-priority', '-due_date', 'created_date']
+
+    def __unicode__(self):
+        # Tell it to return as a unicode string (The name of the to-do item) rather than just Object.
+        return self.title
